27980233bfb6ad6c68105a2fb121787e0747fc39
yg4798@gmail.com <yg4798@gmail.com>
2025-06-15 12:22:53 +0530

    Created Express Server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express= require('express');
+
+const app= express()
+
+app.use("/", (req, res) => {
+  res.send("Hello from the Home Page");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello from the Heelo Page in Application");
+});
+app.use("/test",(req,res)=>{
+  res.send("Hello from the server")
+})
+
+app.listen(3000,()=>{
+  console.log("Server is Running ")
+})
