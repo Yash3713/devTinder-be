134dc495abca1c822caf923a548a090b767c53ca
yg4798@gmail.com <yg4798@gmail.com>
2025-06-17 23:05:29 +0530

    DB validations & Api (patch /user)  validations

--- src/app.js
+++ src/app.js
@@ -1,54 +1,91 @@
 const express = require("express");
 
 const app = express();
-const connectDB= require("./config/database")
-const User= require("./models/user")
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/signup", async (req,res)=>{
+app.post("/signup", async (req, res) => {
   //console.log(req.body)
   //Creating a new instance of User Model
   const user = new User(req.body);
-  try{
-    await user.save()
-    res.send("User Saved !!")
-  }catch(err){
-    res.status(400).send("Error Occured in Saving User :"+err.message)
-  }
- 
-})
-
-app.get("/user",async(req,res)=>{
-  const userEmail= req.body.emailId
-  try{
-    const user=  await User.find({emailId: userEmail}) 
-    if(user.length===0){
-res.status(404).send("User Not Found")
-    }else{res.send(user);}
-    
-  }catch(err){
-    console.log("Something Went Wrong")
-  }
-})
-
-app.get("/feed",async(req,res)=>{
   try {
-    const users =  await User.find({});
+    await user.save();
+    res.send("User Saved !!");
+  } catch (err) {
+    res.status(400).send("Error Occured in Saving User :" + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    console.log("Something Went Wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
     res.send(users);
   } catch (err) {
     console.log("Something Went Wrong");
   }
-  
-
-})
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  if (!userId) {
+    req.status(404).send("UserId is Mandatory ");
+  }
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    console.log("Something Went Wrong" + err.message);
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+ 
+  try {
+    const ALLOWED_UPDATES = ["userId","about", "gender", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update Not allowed");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User Updated Succesfully" + user);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ error: "Something went wrong", details: err.message });
+    console.log("Something Went Wrong" + err.message);
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database Connected");
     app.listen(3000, () => {
       console.log("Server is Running ");
     });
-    
   })
   .catch((err) => {
     console.error("Database Connection Failed");
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,84 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      maxLength: 20,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender not Valid !");
+        }
+      },
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        },
+        message: "Invalid email format",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/.test(value);
+        },
+        message:
+          "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character",
+      },
+    },
+    skills: {
+      type: [String],
+      validate: {
+        validator: function (value) {
+          return value.length <= 5;
+        },
+        message: "Maximum Limit is 5",
+      },
+      validate: {
+        validator: function (value) {
+          const duplicates = value.filter(
+            (i, index) => value.indexOf(i) !== index
+          );
+          return duplicates.length === 0;
+        },
+        message:"No Duplicates Allowed "
+      },
+    },
+    about: {
+      type: String,
+      default: "Default Bio",
+      minLength: 10,
+      maxLength: 70,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const User= mongoose.model("User", userSchema)
-module.exports= User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
